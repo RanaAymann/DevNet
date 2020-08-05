e7ea37955aabe617e996f4a12d8f4074f39d2176
Rana Ayman <33266962+ranaelemam@users.noreply.github.com>
2020-08-05 12:41:52 +0300

    Update bash1.sh

--- bash1.sh
+++ bash1.sh
@@ -10,6 +10,60 @@
  
  source py3-venv/bin/activate
  
+ deactivate
+ 
+ 
+ sudo apt install nodejs
+ 
+ sudo apt install npm
+ 
+ nodejs -v
+ 
+ ## Applocations need :
+ 
+ sudo snap install postman
+ 
+ snap install ngrok
+ 
+ # Example :
+ 
+ ngrok http 5000
+ 
+ sudo apt install openconnect
+ 
+  # Example Connection
+ sudo openconnect -b dcloud-rtp-anyconnect.cisco.com
+ 
+ # Find the running process
+ sudo ps -ax | grep openconnect
+ 
+ sudo kill 22741 
+ 
+ # Application container engine
+ 
+ sudo apt install apt-transport-https ca-certificates curl software-properties-common
+ 
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  
+ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+ 
+ sudo apt update
+ sudo apt install docker-ce
+ 
+ sudo systemctl status docker
+  
+ sudo usermod -aG docker $USER
+   
+ docker run busybox
+    
+ docker ps -a
+  
+  
+  
+  
+ 
+ 
+ 
  
  
   

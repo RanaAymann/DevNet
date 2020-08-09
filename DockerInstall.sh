588bd5a2b4d5c97df8164879a1fb822739ee3aba
Rana Ayman <33266962+ranaelemam@users.noreply.github.com>
2020-08-09 14:59:49 +0300

    Create DockerInstall.sh

---
+++ DockerInstall.sh
@@ -0,0 +1,13 @@
+docker -v
+
+# search for images on DockerHub
+
+docker seravh hello-world
+
+#Docker first checks to see whether the "hello-world" image is available locally. If not, Docker automatically downloads it from DockerHub.
+Docker sets up the container to run locally,including ensuring its isolation from other processes. Once the preparations are made, Docker runs the image.
+
+docker run hello-world
+
+
+# create docker image 
